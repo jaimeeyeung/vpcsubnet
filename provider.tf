8dd976a79daf5b701ac543b0e352b2cea6dd602a
Jaimee Yeung <68402647+jaimeeyeung@users.noreply.github.com>
2021-04-28 14:06:58 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ instance_tenancy = "default"
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id     = "${aws_vpc.main.id}"
+  vpc_id     = aws_vpc.main.id
   cidr_block = "190.160.1.0/24"
 
   tags = {
